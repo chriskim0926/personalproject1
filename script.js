f24bb1192e523c73cdd725bbf9acdf63a36f85a5
chris kim <chriskim0926@gmail.com>
2020-10-16 14:18:31 -0400

    add new

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    
+
+    function superhero() {
+        var queryURL = "https://superheroapi.com/api.php/10164273699360858/search/batman"
+        $.ajax({
+          url: queryURL,
+          method: "GET",
+        }).then(function (response) {
+            console.log(response)
+            console.log(response.results[0].image.url)
+            console.log(response.results[0].biography)
+            var image = document.getElementById("characterimage")
+            var imageURL = response.results[0].image.url
+            var name = response.results[0].name
+
+            //Variable need for the Character Details
+            var description = response.results[0].biography
+
+            $('#characterimage').append("<img src=" +imageURL+"></img>")
+            $('#characterName').append("<h1>"+name+"</h1>")
+            $('#characterDetails').append("<p>"+description+"</p>")
+            $('#characterDetails').append("<p>"+appearance+"</p>")
+            
+        });
+      }
+    superhero()
+    })
+  
+  //VARIABLES
+
+  //Number entered by user.
+  //4 is just a placeholder until we create the input and get the value.
+  var userNumber = 4;
+
+
+  //FUNCTIONS
+  
+  //Function to get the superhero from the superhero api by id
+  //Must be between 1 and 731
+  function getSuperHero(id) {
+    var queryURL = "https://superheroapi.com/api.php/10164273699360858/" + id;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var name = response.name;
+
+      getBooks(name);
+
+    });
+  }
+
+  //Function to get books and comics related to the superhero from goodreads api
+  function getBooks(name) {
+    var queryURL = "https://v1.nocodeapi.com/shelboc/gr/dIBrccmAYkfwiAFv/search?q=" + name;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var books = response.results;
+
+    });
+  }
+
+  // FUNCTION CALLS
+  getSuperHero(userNumber);
+
+  // EVENT LISTENERS
